aaaccd8812f25a0aa1df656a8b5d177e96edd8cd
MY NGUYỄN <mymy42us@gmail.com>
2020-01-06 18:10:52 +0700

    + xin nâng cấp, chấp nhận nâng cấp, hạ cấp

--- src/routes/admin/category.route.js
+++ src/routes/admin/category.route.js
@@ -5,32 +5,32 @@ const router = express.Router();
 
 router.get('/', async(req, res) => {
     const rows = await categoryModel.all();
-    res.render('vwCategories/index', {
+    res.render('vwAdmin/vwCategories/index', {
         categories: rows,
         empty: rows.length === 0
     });
 })
 
 router.get('/add', (req, res) => {
-    res.render('vwCategories/add');
+    res.render('vwAdmin/vwCategories/add');
 })
 
 router.post('/add', async(req, res) => {
     const result = await categoryModel.add(req.body);
-    res.render('vwCategories/add');
+    res.render('vwAdmin/vwCategories/add');
 })
 
 router.get('/err', (req, res) => {
     throw new Error('error occured');
 })
 
-router.get('/edit/:id', async(req, res) => {
+router.get('/:id/edit', async(req, res) => {
     const rows = await categoryModel.single(req.params.id);
     if (rows.length === 0) {
         throw new Error('Invalid category id');
     }
 
-    res.render('vwCategories/edit', {
+    res.render('vwAdmin/vwCategories/edit', {
         category: rows[0]
     });
 })
@@ -45,23 +45,23 @@ router.post('/del', async(req, res) => {
     res.redirect('/admin/categories');
 })
 
-router.get('/detail/:id', async(req, res) => {
+router.get('/:id/detail', async(req, res) => {
     try {
         // const rows = await db.load('select * from categories');
         const rows = await categoryModel.detai(+req.params.id);
-        res.render('vwCategories/detail', {
+        res.render('vwAdmin/vwCategories/detail', {
             categories: rows,
             empty: rows.length === 0
         });
     } catch (err) {
         console.log(err);
-        res.end('View error log in console.');
+        //res.end('View error log in console.');
+        res.render('error', { layout: false });
     }
 })
 
-router.post('/detail/:ProductID/delete', async(req, res) => {
-    const result = await categoryModel.delP(req.body.ProductID);
-    //console.log(result);
+router.get('/:cateID/detail/:productID/delete', async(req, res) => {
+    const result = await categoryModel.delP(req.params.productID);
     res.redirect('/admin/categories');
 })
 
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -4,13 +4,13 @@ const config = require('../config/default.json');
 
 const router = express.Router();
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async(req, res) => {
 
-  const proId = req.params.id;
-  const rows = await productModel.single(proId);
-  res.render('vwProducts/detail', {
-    product: rows[0]
-  });
+    const proId = req.params.id;
+    const rows = await productModel.single(proId);
+    res.render('vwProducts/detail', {
+        product: rows[0]
+    });
 })
 
 
